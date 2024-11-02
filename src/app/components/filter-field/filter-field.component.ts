fa91f6875102c2036171fe7e69715484b240f9b4
Emanuel Douglas Sousa Costa <emanuel.douglas.sc@gmail.com>
2024-11-02 14:32:37 -0300

    layout do campo de pesquisa

--- src/app/components/filter-field/filter-field.component.ts
+++ src/app/components/filter-field/filter-field.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lore-filter-field',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './filter-field.component.html',
   styleUrl: './filter-field.component.sass'
 })
 export class FilterFieldComponent {
+  searchTerm: string = '';
 
+  @Output() search = new EventEmitter<string>();
+
+  onSearch() {
+    this.search.emit(this.searchTerm);
+  }
 }
